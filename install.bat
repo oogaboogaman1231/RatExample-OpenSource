c726f078b5bf8e74ed48d44cb2701f2fe2152f5d
oogaboogaman1231 <originalityklan@gmail.com>
2024-06-04 22:42:22 -0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,12 +1,11 @@
 @echo off
 setlocal
 
-REM Set the paths for your compiler and source files
-set "COMPILER_PATH=C:\Path\To\Your\Compiler"
+REM Set the paths for your source files
 set "SRC_PATH=%~dp0\src"
-set "OUTPUT_DIR=%~dp0\bin"
+set "OUTPUT_DIR=%ProgramFiles%\RatExample"
 set "EXECUTABLE_NAME=rat_server.exe"
-set "SHORTCUT_NAME=RAT Server.lnk"
+set "SHORTCUT_NAME=RatExample.lnk"
 set "DESKTOP_PATH=%USERPROFILE%\Desktop"
 
 REM Create the output directory if it doesn't exist
@@ -14,8 +13,12 @@ if not exist "%OUTPUT_DIR%" (
     mkdir "%OUTPUT_DIR%"
 )
 
-REM Compile the project
-"%COMPILER_PATH%\cl.exe" /EHsc /Fe:"%OUTPUT_DIR%\%EXECUTABLE_NAME%" "%SRC_PATH%\main.cpp" ws2_32.lib Comctl32.lib
+REM Download all necessary files to the output directory
+
+REM Wait for files to be downloaded (you may need to add some logic here)
+
+REM Compile the project (using the default Windows compiler)
+cl.exe /EHsc /Fe:"%OUTPUT_DIR%\%EXECUTABLE_NAME%" "%OUTPUT_DIR%\rat_server.cpp" ws2_32.lib Comctl32.lib
 
 REM Check if the compilation was successful
 if errorlevel 1 (
@@ -29,6 +32,7 @@ echo Set oWS = WScript.CreateObject("WScript.Shell") > "%TEMP%\CreateShortcut.vb
 echo sLinkFile = "%DESKTOP_PATH%\%SHORTCUT_NAME%" >> "%TEMP%\CreateShortcut.vbs"
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> "%TEMP%\CreateShortcut.vbs"
 echo oLink.TargetPath = "%OUTPUT_DIR%\%EXECUTABLE_NAME%" >> "%TEMP%\CreateShortcut.vbs"
+echo oLink.IconLocation = "%OUTPUT_DIR%\Rat.ico" >> "%TEMP%\CreateShortcut.vbs"
 echo oLink.Save >> "%TEMP%\CreateShortcut.vbs"
 
 cscript /nologo "%TEMP%\CreateShortcut.vbs"
